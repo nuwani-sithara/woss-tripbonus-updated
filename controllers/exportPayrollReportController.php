813e19fa4db16b434d144deaa395320cd647ae68
nuwani-sithara <it22099518@my.sliit.lk>
2025-08-31 21:02:17 +0530

    added emails

--- controllers/exportPayrollReportController.php
+++ controllers/exportPayrollReportController.php
@@ -233,9 +233,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['
     $payments = getPayments($conn, $month, $year);
     $totals = getTotals($conn, $month, $year);
     // Email logic using PHPMailer
-    $recipients = 'lahiru@mclarens.lk, nuwanisithara.com@gmail.com, devindi@mclarens.lk';
+    $recipients = 'sajiths@mclarens.lk, mark@worldsubsea.lk, anton@worldsubsea.lk, lahiru@mclarens.lk';
     $to = 'lahiru@mclarens.lk';
-    $cc = ['nuwanisithara.com@gmail.com', 'devindi@mclarens.lk'];
+    $cc = ['sajiths@mclarens.lk', 'mark@worldsubsea.lk', 'anton@worldsubsea.lk'];
     $subject = "Payroll Report for $month $year";
     $headers = ['Employee', 'Job Allowance', 'Job Meal', 'Standby Attendance', 'Standby Meal', 'Report Prep', 'Total Diving', 'Date & Time'];
     $rows = [];
